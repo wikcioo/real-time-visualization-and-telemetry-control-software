dab3f9a903bb57d696505f789149e5b8504d3ea8
wiktor <wikcioo@gmail.com>
2022-09-19 20:36:50 +0200

    Added a hash map of entities and initialized it with a square entity. All entities are drawn in the scene's draw function.

--- include/scene_view.hpp
+++ include/scene_view.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <map>
 #include <memory>
 #include "camera.hpp"
+#include "entity.hpp"
 #include "opengl_buffer_manager.hpp"
 
 class SceneView
@@ -11,10 +13,14 @@ public:
 
     void ProcessMouseMovement(double xChange, double yChange, int button);
 
+    std::shared_ptr<Entity> GetEntity(const std::string& name) { return m_Entities.at(name); }
     void SetSize(int width, int height);
     void Draw();
 private:
     float m_Width, m_Height;
     std::unique_ptr<PerspectiveCamera> m_Camera;
     std::unique_ptr<renderer::GLFrameBuffer> m_Framebuffer;
+    std::map<std::string, std::shared_ptr<Entity>> m_Entities;
+private:
+    void InitializeEntities();
 };
--- src/scene_view.cpp
+++ src/scene_view.cpp
@@ -8,6 +8,8 @@ SceneView::SceneView(int width, int height)
     m_Camera = std::make_unique<PerspectiveCamera>(glm::vec3(0, 0, 2), glm::vec3(0, 0, -1), m_Width / m_Height);
     m_Framebuffer = std::make_unique<renderer::GLFrameBuffer>();
     m_Framebuffer->CreateBuffers(m_Width, m_Height);
+
+    InitializeEntities();
 }
 
 SceneView::~SceneView()
@@ -28,12 +30,33 @@ void SceneView::SetSize(int width, int height)
     m_Framebuffer->CreateBuffers(m_Width, m_Height);
 }
 
+void SceneView::InitializeEntities()
+{
+    std::vector<Vertex> vertices;
+
+    vertices.push_back({glm::vec3(-0.5f, -0.5f, 0.0f)});
+    vertices.push_back({glm::vec3( 0.5f, -0.5f, 0.0f)});
+    vertices.push_back({glm::vec3( 0.5f,  0.5f, 0.0f)});
+    vertices.push_back({glm::vec3(-0.5f,  0.5f, 0.0f)});
+
+    std::vector<unsigned int> indices{0, 1, 2, 2, 3, 0};
+
+    std::shared_ptr<Entity> e1 = std::make_shared<Entity>();
+    e1->Initialize(vertices, indices, "res/shaders/basic_vs.glsl", "res/shaders/basic_fs.glsl");
+    m_Entities.insert({"square", e1});
+}
+
 void SceneView::Draw()
 {
     m_Framebuffer->Bind();
 
     glClearColor(0.4f, 0.2f, 0.7f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    for (auto entity : m_Entities)
+    {
+        entity.second->Draw(m_Camera->GetViewProjection());
+    }
 
     m_Framebuffer->Unbind();
 
