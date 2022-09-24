5da6ea7c9d8fb19e36b4f15523925a16509c5d16
wiktor <wikcioo@gmail.com>
2022-09-24 16:39:48 +0200

    Moved to 1920x1080 resolution.

--- src/application.cpp
+++ src/application.cpp
@@ -2,7 +2,7 @@
 
 Application::Application(const std::string& title)
 {
-    m_Window = std::make_unique<GLWindow>(1280, 720, title);
+    m_Window = std::make_unique<GLWindow>(1920, 1080, title);
     m_Window->Initialize();
 }
 
