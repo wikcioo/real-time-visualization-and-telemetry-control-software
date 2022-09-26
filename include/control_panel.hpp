71c2cfda6c99c983ffd2dec015266b98cb2cc115
wiktor <wikcioo@gmail.com>
2022-09-27 00:06:13 +0200

    Refactored serial log panel. Now possible to connect and disconnect from any valid serial device.

--- include/control_panel.hpp
+++ include/control_panel.hpp
@@ -13,4 +13,19 @@ public:
     void Draw(std::shared_ptr<SceneView> scene);
 private:
     std::unique_ptr<serial::SerialPort> m_SerialPort;
+    void setSerialDevice(const char* dev);
+    struct SerialLog
+    {
+        ControlPanel* Cp;
+        ImGuiTextBuffer Buf;
+        ImVector<int> LineOffsets;
+        bool AutoScroll;
+        bool Connected;
+
+        SerialLog(ControlPanel* cp);
+        void Clear();
+        void AddLog(const char* fmt, ...) IM_FMTARGS(2);
+        void Draw(const char* title, bool* p_open = NULL);
+    } m_LogPanel;
+
 };
--- src/control_panel.cpp
+++ src/control_panel.cpp
@@ -1,138 +1,127 @@
 #include "control_panel.hpp"
 #include <GL/glew.h>
 
-struct ExampleAppLog
+ControlPanel::SerialLog::SerialLog(ControlPanel* cp)
 {
-    ImGuiTextBuffer     Buf;
-    ImGuiTextFilter     Filter;
-    ImVector<int>       LineOffsets; // Index to lines offset. We maintain this with AddLog() calls.
-    bool                AutoScroll;  // Keep scrolling if already at the bottom.
-
-    ExampleAppLog()
-    {
-        AutoScroll = true;
-        Clear();
-    }
-
-    void    Clear()
-    {
-        Buf.clear();
-        LineOffsets.clear();
-        LineOffsets.push_back(0);
-    }
-
-    void    AddLog(const char* fmt, ...) IM_FMTARGS(2)
-    {
-        int old_size = Buf.size();
-        va_list args;
-        va_start(args, fmt);
-        Buf.appendfv(fmt, args);
-        va_end(args);
-        for (int new_size = Buf.size(); old_size < new_size; old_size++)
-            if (Buf[old_size] == '\n')
-                LineOffsets.push_back(old_size + 1);
-    }
-
-    void    Draw(const char* title, bool* p_open = NULL)
-    {
-        if (!ImGui::Begin(title, p_open))
-        {
-            ImGui::End();
-            return;
-        }
-
-        // Options menu
-        if (ImGui::BeginPopup("Options"))
-        {
-            ImGui::Checkbox("Auto-scroll", &AutoScroll);
-            ImGui::EndPopup();
-        }
-
-        // Main window
-        if (ImGui::Button("Options"))
-            ImGui::OpenPopup("Options");
-        ImGui::SameLine();
-        bool clear = ImGui::Button("Clear");
-        ImGui::SameLine();
-        bool copy = ImGui::Button("Copy");
-        ImGui::SameLine();
-        Filter.Draw("Filter", -100.0f);
-
-        ImGui::Separator();
-        ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
-
-        if (clear)
-            Clear();
-        if (copy)
-            ImGui::LogToClipboard();
-
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
-        const char* buf = Buf.begin();
-        const char* buf_end = Buf.end();
-        if (Filter.IsActive())
-        {
-            // In this example we don't use the clipper when Filter is enabled.
-            // This is because we don't have a random access on the result on our filter.
-            // A real application processing logs with ten of thousands of entries may want to store the result of
-            // search/filter.. especially if the filtering function is not trivial (e.g. reg-exp).
-            for (int line_no = 0; line_no < LineOffsets.Size; line_no++)
-            {
-                const char* line_start = buf + LineOffsets[line_no];
-                const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
-                if (Filter.PassFilter(line_start, line_end))
-                    ImGui::TextUnformatted(line_start, line_end);
-            }
-        }
-        else
-        {
-            // The simplest and easy way to display the entire buffer:
-            //   ImGui::TextUnformatted(buf_begin, buf_end);
-            // And it'll just work. TextUnformatted() has specialization for large blob of text and will fast-forward
-            // to skip non-visible lines. Here we instead demonstrate using the clipper to only process lines that are
-            // within the visible area.
-            // If you have tens of thousands of items and their processing cost is non-negligible, coarse clipping them
-            // on your side is recommended. Using ImGuiListClipper requires
-            // - A) random access into your data
-            // - B) items all being the  same height,
-            // both of which we can handle since we an array pointing to the beginning of each line of text.
-            // When using the filter (in the block of code above) we don't have random access into the data to display
-            // anymore, which is why we don't use the clipper. Storing or skimming through the search result would make
-            // it possible (and would be recommended if you want to search through tens of thousands of entries).
-            ImGuiListClipper clipper;
-            clipper.Begin(LineOffsets.Size);
-            while (clipper.Step())
-            {
-                for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
-                {
-                    const char* line_start = buf + LineOffsets[line_no];
-                    const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
-                    ImGui::TextUnformatted(line_start, line_end);
-                }
-            }
-            clipper.End();
-        }
-        ImGui::PopStyleVar();
-
-        if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
-            ImGui::SetScrollHereY(1.0f);
-
-        ImGui::EndChild();
-        ImGui::End();
-    }
+    Cp = cp;
+    AutoScroll = true;
+    Connected = false;
+    Clear();
 };
 
-ControlPanel::ControlPanel()
+void ControlPanel::SerialLog::Clear()
 {
-    try {
-        m_SerialPort = std::make_unique<serial::SerialPort>("/dev/ttyACM0");
-    } catch (serial::SerialPortException& e) {
-        std::cout << e.what();
-        m_SerialPort = nullptr;
+    Buf.clear();
+    LineOffsets.clear();
+    LineOffsets.push_back(0);
+};
+
+void ControlPanel::SerialLog::AddLog(const char* fmt, ...)
+{
+    int old_size = Buf.size();
+    va_list args;
+    va_start(args, fmt);
+    Buf.appendfv(fmt, args);
+    va_end(args);
+    for (int new_size = Buf.size(); old_size < new_size; old_size++)
+        if (Buf[old_size] == '\n')
+            LineOffsets.push_back(old_size + 1);
+};
+
+void ControlPanel::SerialLog::Draw(const char* title, bool* p_open)
+{
+    if (!ImGui::Begin(title, p_open))
+    {
+        ImGui::End();
+        return;
     }
+
+    // Options menu
+    if (ImGui::BeginPopup("Options"))
+    {
+        ImGui::Checkbox("Auto-scroll", &AutoScroll);
+        ImGui::EndPopup();
+    }
+
+    // Main window
+    if (ImGui::Button("Options"))
+        ImGui::OpenPopup("Options");
+    ImGui::SameLine();
+    bool clear = ImGui::Button("Clear");
+    ImGui::SameLine();
+    bool copy = ImGui::Button("Copy");
+    //ImGui::SameLine();
+    static char port_buffer[32] = "";
+    ImGui::InputText("port", port_buffer, 32, ImGuiInputTextFlags_CharsNoBlank);
+    ImGui::SameLine();
+    bool connection = ImGui::Button(Connected ? "Disconnect" : "Connect");
+
+    ImGui::Separator();
+    ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+
+    if (clear)
+        Clear();
+    if (copy)
+        ImGui::LogToClipboard();
+    if (connection && !Connected)
+    {
+        const char *dev_start = "/dev/tty";
+        if (strlen(port_buffer) > strlen(dev_start) && strncmp(dev_start, port_buffer, strlen(dev_start)) == 0) {
+            Cp->setSerialDevice(port_buffer);
+        } else {
+            AddLog("Not a teletype\n");
+        }
+    }
+    else if (connection && Connected)
+    {
+        Connected = false;
+        Cp->m_SerialPort = nullptr;
+        AddLog("Disconnected from %s\n", port_buffer);
+    }
+
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+    const char* buf = Buf.begin();
+    const char* buf_end = Buf.end();
+    ImGuiListClipper clipper;
+    clipper.Begin(LineOffsets.Size);
+    while (clipper.Step())
+    {
+        for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
+        {
+            const char* line_start = buf + LineOffsets[line_no];
+            const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+            ImGui::TextUnformatted(line_start, line_end);
+        }
+    }
+    clipper.End();
+    ImGui::PopStyleVar();
+
+    if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+        ImGui::SetScrollHereY(1.0f);
+
+    ImGui::EndChild();
+    ImGui::End();
+}
+
+ControlPanel::ControlPanel()
+    : m_SerialPort(nullptr), m_LogPanel{this}
+{
 }
 
 ControlPanel::~ControlPanel()
 {
+}
+
+void ControlPanel::setSerialDevice(const char* dev)
+{
+    try {
+        m_SerialPort = std::make_unique<serial::SerialPort>(dev);
+        m_LogPanel.AddLog("Successfully connected to %s\n", dev);
+        m_LogPanel.Connected = true;
+    } catch (serial::SerialPortException& e) {
+        m_LogPanel.AddLog("Failed to connect to %s\nError: %s\n", dev, e.what());
+        m_LogPanel.Connected = false;
+    }
 }
 
 void ControlPanel::Draw(std::shared_ptr<SceneView> scene)
@@ -183,15 +172,14 @@ void ControlPanel::Draw(std::shared_ptr<SceneView> scene)
     plane->SetColor({cube_color.x, cube_color.y, cube_color.z});
 
     // Serial Log
-    static ExampleAppLog log;
     static bool p_open;
     ImGui::Begin("Serial Log", &p_open);
     if (m_SerialPort && m_SerialPort->IsDataReady()) {
         uint8_t buffer[32];
         memset(buffer, 0, 32);
         m_SerialPort->ReadData((uint8_t*)buffer, 32);
-        log.AddLog("%s", buffer);
+        m_LogPanel.AddLog("%s", buffer);
     }
     ImGui::End();
-    log.Draw("Serial Log", &p_open);
+    m_LogPanel.Draw("Serial Log", &p_open);
 }
