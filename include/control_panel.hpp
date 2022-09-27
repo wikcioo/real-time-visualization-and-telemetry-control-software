30f469f77bc9d06c726323a867fc4ba52e7307dc
wiktor <wikcioo@gmail.com>
2022-09-27 02:49:33 +0200

    Added a serial console panel. Made the serial log panel only display raw serial data in text form. Added 4 console commands: help, clear, connect <device>, disconnect. Removed a dummy metrics window.

--- include/control_panel.hpp
+++ include/control_panel.hpp
@@ -13,14 +13,14 @@ public:
     void Draw(std::shared_ptr<SceneView> scene);
 private:
     std::unique_ptr<serial::SerialPort> m_SerialPort;
-    void setSerialDevice(const char* dev);
+    bool m_IsSerialDeviceConnected;
+
     struct SerialLog
     {
         ControlPanel* Cp;
         ImGuiTextBuffer Buf;
         ImVector<int> LineOffsets;
         bool AutoScroll;
-        bool Connected;
 
         SerialLog(ControlPanel* cp);
         void Clear();
@@ -28,4 +28,32 @@ private:
         void Draw(const char* title, bool* p_open = NULL);
     } m_LogPanel;
 
+    struct SerialConsole
+    {
+        ControlPanel* Cp;
+        char InputBuf[256];
+        ImVector<char*> Items;
+        ImVector<const char*> Commands;
+        bool AutoScroll;
+        bool ScrollToBottom;
+
+        SerialConsole(ControlPanel* cp);
+        ~SerialConsole();
+
+        // Portable helpers
+        static int   Stribeg(const char* s1, const char* s2)         { return strncmp(s1, s2, strlen(s2)) == 0; }
+        static int   Stricmp(const char* s1, const char* s2)         { int d; while ((d = toupper(*s2) - toupper(*s1)) == 0 && *s1) { s1++; s2++; } return d; }
+        static int   Strnicmp(const char* s1, const char* s2, int n) { int d = 0; while (n > 0 && (d = toupper(*s2) - toupper(*s1)) == 0 && *s1) { s1++; s2++; n--; } return d; }
+        static char* Strdup(const char* s)                           { IM_ASSERT(s); size_t len = strlen(s) + 1; void* buf = malloc(len); IM_ASSERT(buf); return (char*)memcpy(buf, (const void*)s, len); }
+        static void  Strtrim(char* s)                                { char* str_end = s + strlen(s); while (str_end > s && str_end[-1] == ' ') str_end--; *str_end = 0; }
+
+        void ClearLog();
+        void AddLog(const char* fmt, ...) IM_FMTARGS(2);
+        void Draw(const char* title, bool* p_open);
+        void ExecCommand(const std::string& command_line);
+    } m_ConsolePanel;
+private:
+    void setSerialDevice(const std::string& dev);
+    void clearSerialDevice();
+    std::string getSerialDeviceName();
 };
--- src/control_panel.cpp
+++ src/control_panel.cpp
@@ -1,11 +1,11 @@
 #include "control_panel.hpp"
 #include <GL/glew.h>
+#include <sstream>
 
 ControlPanel::SerialLog::SerialLog(ControlPanel* cp)
 {
     Cp = cp;
     AutoScroll = true;
-    Connected = false;
     Clear();
 };
 
@@ -50,11 +50,6 @@ void ControlPanel::SerialLog::Draw(const char* title, bool* p_open)
     bool clear = ImGui::Button("Clear");
     ImGui::SameLine();
     bool copy = ImGui::Button("Copy");
-    //ImGui::SameLine();
-    static char port_buffer[32] = "";
-    ImGui::InputText("port", port_buffer, 32, ImGuiInputTextFlags_CharsNoBlank);
-    ImGui::SameLine();
-    bool connection = ImGui::Button(Connected ? "Disconnect" : "Connect");
 
     ImGui::Separator();
     ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
@@ -63,21 +58,6 @@ void ControlPanel::SerialLog::Draw(const char* title, bool* p_open)
         Clear();
     if (copy)
         ImGui::LogToClipboard();
-    if (connection && !Connected)
-    {
-        const char *dev_start = "/dev/tty";
-        if (strlen(port_buffer) > strlen(dev_start) && strncmp(dev_start, port_buffer, strlen(dev_start)) == 0) {
-            Cp->setSerialDevice(port_buffer);
-        } else {
-            AddLog("Not a teletype\n");
-        }
-    }
-    else if (connection && Connected)
-    {
-        Connected = false;
-        Cp->m_SerialPort = nullptr;
-        AddLog("Disconnected from %s\n", port_buffer);
-    }
 
     ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
     const char* buf = Buf.begin();
@@ -103,8 +83,161 @@ void ControlPanel::SerialLog::Draw(const char* title, bool* p_open)
     ImGui::End();
 }
 
+ControlPanel::SerialConsole::SerialConsole(ControlPanel* cp)
+{
+    Cp = cp;
+    AutoScroll = true;
+    ScrollToBottom = false;
+
+    ClearLog();
+    memset(InputBuf, 0, sizeof(InputBuf));
+
+    Commands.push_back("help");
+    Commands.push_back("clear");
+    Commands.push_back("connect <device>");
+    Commands.push_back("disconnect");
+}
+
+ControlPanel::SerialConsole::~SerialConsole()
+{
+    ClearLog();
+}
+
+void ControlPanel::SerialConsole::ClearLog()
+{
+    for (int i = 0; i < Items.Size; i++)
+        free(Items[i]);
+    Items.clear();
+}
+
+void ControlPanel::SerialConsole::AddLog(const char* fmt, ...)
+{
+    // FIXME-OPT
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buf, IM_ARRAYSIZE(buf), fmt, args);
+    buf[IM_ARRAYSIZE(buf)-1] = 0;
+    va_end(args);
+    Items.push_back(Strdup(buf));
+}
+
+void ControlPanel::SerialConsole::Draw(const char* title, bool* p_open)
+{
+    ImGui::SetNextWindowSize(ImVec2(520, 600), ImGuiCond_FirstUseEver);
+    if (!ImGui::Begin(title, p_open))
+    {
+        ImGui::End();
+        return;
+    }
+
+    std::string status = Cp->m_IsSerialDeviceConnected ? "connected to " + Cp->getSerialDeviceName() : "disconnected";
+    ImGui::Text("Status: %s", status.c_str());
+    ImGui::Separator();
+
+    // Reserve enough left-over height for 1 separator + 1 input text
+    const float footer_height_to_reserve = ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
+    ImGui::BeginChild("ScrollingRegion", ImVec2(0, -footer_height_to_reserve), false, ImGuiWindowFlags_HorizontalScrollbar);
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1)); // Tighten spacing
+    for (int i = 0; i < Items.Size; i++)
+    {
+        const char* item = Items[i];
+        ImVec4 color;
+        bool has_color = false;
+        if (strstr(item, "[error]"))          { color = ImVec4(1.0f, 0.4f, 0.4f, 1.0f); has_color = true; }
+        else if (strncmp(item, "# ", 2) == 0) { color = ImVec4(1.0f, 0.8f, 0.6f, 1.0f); has_color = true; }
+        if (has_color)
+            ImGui::PushStyleColor(ImGuiCol_Text, color);
+        ImGui::TextUnformatted(item);
+        if (has_color)
+            ImGui::PopStyleColor();
+    }
+
+    if (ScrollToBottom || (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY()))
+        ImGui::SetScrollHereY(1.0f);
+    ScrollToBottom = false;
+
+    ImGui::PopStyleVar();
+    ImGui::EndChild();
+    ImGui::Separator();
+
+    // Command-line
+    bool reclaim_focus = false;
+    ImGuiInputTextFlags input_text_flags = ImGuiInputTextFlags_EnterReturnsTrue;
+    if (ImGui::InputText("Input", InputBuf, IM_ARRAYSIZE(InputBuf), input_text_flags))
+    {
+        char* s = InputBuf;
+        Strtrim(s);
+        if (s[0])
+            ExecCommand(s);
+        strcpy(s, "");
+        reclaim_focus = true;
+    }
+
+    // Auto-focus on window apparition
+    ImGui::SetItemDefaultFocus();
+    if (reclaim_focus)
+        ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
+
+    ImGui::End();
+}
+
+void ControlPanel::SerialConsole::ExecCommand(const std::string& command_line)
+{
+    AddLog("# %s\n", command_line.c_str());
+
+    // Split command line into tokens
+    std::vector<std::string> tokens;
+    std::istringstream ss(command_line);
+    std::string token;
+    while (ss >> token) tokens.push_back(token);
+
+    // Process command
+    if (Stricmp(command_line.c_str(), "clear") == 0)
+    {
+        ClearLog();
+    }
+    else if (Stricmp(command_line.c_str(), "help") == 0)
+    {
+        AddLog("Commands:");
+        for (int i = 0; i < Commands.Size; i++)
+            AddLog("- %s", Commands[i]);
+    }
+    else if (tokens.at(0) == "connect")
+    {
+        if (tokens.size() < 2)
+        {
+            AddLog("[error] wrong usage of the 'connect' command\n");
+        }
+        else
+        {
+            const char *device_prefix = "/dev/tty";
+            if (strncmp(tokens.at(1).c_str(), device_prefix, strlen(device_prefix)) == 0)
+            {
+                Cp->setSerialDevice(tokens.at(1));
+            }
+            else
+            {
+                AddLog("Not a teletype\n");
+            }
+        }
+    }
+    else if (tokens.at(0) == "disconnect")
+    {
+        AddLog("Disconnected from %s\n", Cp->getSerialDeviceName().c_str());
+        Cp->clearSerialDevice();
+    }
+    else
+    {
+        AddLog("Unknown command: '%s'\n", command_line.c_str());
+    }
+
+    // On command input, we scroll to bottom even if AutoScroll==false
+    ScrollToBottom = true;
+}
+
 ControlPanel::ControlPanel()
-    : m_SerialPort(nullptr), m_LogPanel{this}
+    : m_SerialPort(nullptr), m_IsSerialDeviceConnected(false), m_LogPanel{this}, m_ConsolePanel{this}
 {
 }
 
@@ -112,24 +245,35 @@ ControlPanel::~ControlPanel()
 {
 }
 
-void ControlPanel::setSerialDevice(const char* dev)
+void ControlPanel::setSerialDevice(const std::string& dev)
 {
     try {
         m_SerialPort = std::make_unique<serial::SerialPort>(dev);
-        m_LogPanel.AddLog("Successfully connected to %s\n", dev);
-        m_LogPanel.Connected = true;
+        m_ConsolePanel.AddLog("Successfully connected to %s\n", dev.c_str());
+        m_IsSerialDeviceConnected = true;
     } catch (serial::SerialPortException& e) {
-        m_LogPanel.AddLog("Failed to connect to %s\nError: %s\n", dev, e.what());
-        m_LogPanel.Connected = false;
+        m_ConsolePanel.AddLog("Failed to connect to %s\nError: %s\n", dev.c_str(), e.what());
+        m_IsSerialDeviceConnected = false;
     }
+}
+
+void ControlPanel::clearSerialDevice()
+{
+    m_SerialPort = nullptr;
+    m_IsSerialDeviceConnected = false;
+}
+
+std::string ControlPanel::getSerialDeviceName()
+{
+    if (m_SerialPort)
+        return m_SerialPort->GetDeviceName();
+    return "unknown";
 }
 
 void ControlPanel::Draw(std::shared_ptr<SceneView> scene)
 {
     static bool demo = true;
     ImGui::ShowDemoWindow(&demo);
-    static bool metrics = true;
-    ImGui::ShowMetricsWindow(&metrics);
     static bool stack_tool = true;
     ImGui::ShowStackToolWindow(&stack_tool);
 
@@ -171,15 +315,17 @@ void ControlPanel::Draw(std::shared_ptr<SceneView> scene)
     cube->SetColor({cube_color.x, cube_color.y, cube_color.z});
     plane->SetColor({cube_color.x, cube_color.y, cube_color.z});
 
-    // Serial Log
-    static bool p_open;
-    ImGui::Begin("Serial Log", &p_open);
+    // Raw Serial Log
+    static bool serial_log_open;
     if (m_SerialPort && m_SerialPort->IsDataReady()) {
         uint8_t buffer[32];
         memset(buffer, 0, 32);
         m_SerialPort->ReadData((uint8_t*)buffer, 32);
         m_LogPanel.AddLog("%s", buffer);
     }
-    ImGui::End();
-    m_LogPanel.Draw("Serial Log", &p_open);
+    m_LogPanel.Draw("Serial Log", &serial_log_open);
+
+    // Serial Console
+    static bool serial_console_open;
+    m_ConsolePanel.Draw("Serial Console", &serial_console_open);
 }
